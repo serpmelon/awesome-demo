00aee04ee8605325993e63599f1f15e9b5b2c3eb
serpmelon <tyn243222791@126.com>
2020-06-09 09:32:48 +0800

    spring demo code

---
+++ spring/src/main/java/com/togo/spring/annotation/jdk/PostFirstService.java
@@ -0,0 +1,28 @@
+package com.togo.spring.annotation.jdk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @Author taiyn
+ * @Description TODO
+ * @Date 8:34 上午 2020/6/9
+ **/
+@Service
+public class PostFirstService {
+
+    @Autowired
+    private PostSecondService secondService;
+
+    @PostConstruct
+    public void print() {
+
+        System.out.println("first " + this.getClass().getName());
+        System.out.println("first " + secondService.getClass().getName());
+        System.out.println("first @@@@@@@@@@@@@");
+        secondService.print();
+        System.out.println("first @@@@@@@@@@@@@@@");
+    }
+}
---
+++ spring/src/test/java/com/togo/spring/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.togo.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
