70654b1d891030c92735ac2774a50209664f3a61
serpmelon <tyn243222791@126.com>
2021-03-02 10:00:39 +0800

    line by line

---
+++ jdk/src/main/java/com/togo/nio/example/LineByLine.java
@@ -0,0 +1,28 @@
+package com.togo.nio.example;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+/**
+ * @author : taiyn
+ * Date    : 2021/3/2
+ * Time    : 8:52 上午
+ * ---------------------------------------
+ * Desc    :
+ */
+public class LineByLine {
+
+    public static void main(String[] args) {
+
+        Path path = Paths.get("./jdk/src/main/resources", "test.txt");
+        System.out.println(path.toAbsolutePath());
+
+        try(Stream<String> lines = Files.lines(path)) {
+
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
